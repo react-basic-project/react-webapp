694200150134c2a1ef3c0a44a5bba4d1d7a8f197
oyg0420 <oyg900420@gmail.com>
2019-12-16 21:44:54 +0900

    react-router-example

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import styled from 'styled-components';
 
-const Home = () => {
+const HomeStyled = styled.div`
+ width: 100%;
+ background-color: #09d3ac;
+ font-size: 36px;
+ font-weight: bold;
+ color: #FFFF;
+ text-align: center;
+`;
+
+const Button = styled(Link)`
+ width: 100%;
+ display: inline-block;
+ background-color: antiquewhite;
+`;
+
+export const Home = () => {
   return (
-    <div>
+    <HomeStyled>
       React Home
-    </div>
+      <Button to="/list">show List</Button>
+    </HomeStyled>
   )
 };
 
+export const List = () => {
+  return (
+    <div>
+      <ul>
+        <li>list 1</li>
+        <li>list 2</li>
+      </ul>
+      <Button to="/">back to the Home</Button>
+    </div>
+  )
+}
+
 export default {
-  Home
+  Home,
+  List,
 };
